e56523613e3eb672620831730afe249f890a468f
Nicolas Silva <nicolas.silva23@inacapmail.cl>
2023-09-25 16:45:35 -0300

    agregar elementos

--- universo.py
+++ universo.py
@@ -1,2 +1,21 @@
-class unidimensional:
-  pass
+from abc import ABC, abstractclassmethod
+class unidimensional(ABC):#aplico Abstraccion
+
+ def __init__(self):
+    self.volumen=0
+    self.masa=0
+    print("Soy Unidimensional")
+    
+class bidimensional(unidimensional):#Aplico Herencia
+   def __init__(self):
+      super().__init__()# LLamado de constructor padre
+      print("Hola, soy un elemento bidimensional")    
+
+
+
+
+if(__name__ == "__main__"):
+   print("Estoy en el programa principal")
+   c = unidimensional()
+   print(c.volumen)
+   
